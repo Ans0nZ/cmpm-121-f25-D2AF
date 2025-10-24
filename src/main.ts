e7dceea9be097b2d5900926157a8252172a68768
Ans0nZ <151815313+Ans0nZ@users.noreply.github.com>
2025-10-24 04:28:50 +0000

    step 1 complete: remove code and comments that are not used, adding the outline of the app(canvas)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <header>
+    <h1 id="app-title">CMPM 121 — My App</h1>
+  </header>
+  <main>
+    <canvas id="game-canvas" class="game-canvas" width="256" height="256"></canvas>
+  </main>
 `;
