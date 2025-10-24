a39c67105858d822b22bf52fd801a359d5fc12f9
Ans0nZ <151815313+Ans0nZ@users.noreply.github.com>
2025-10-24 04:41:16 +0000

    step 2: -drawing function is complete by add mouse event

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,55 @@ document.body.innerHTML = `
     <canvas id="game-canvas" class="game-canvas" width="256" height="256"></canvas>
   </main>
 `;
+
+const canvas = document.getElementById("game-canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d")!;
+
+// types of pen
+ctx.lineWidth = 4;
+ctx.lineCap = "round";
+ctx.lineJoin = "round";
+ctx.strokeStyle = "#222";
+
+// drawing
+let drawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function getPos(e: MouseEvent) {
+  const rect = canvas.getBoundingClientRect();
+  return {
+    x: e.clientX - rect.left,
+    y: e.clientY - rect.top,
+  };
+}
+
+// mouse event
+canvas.addEventListener("mousedown", (e) => {
+  drawing = true;
+  const { x, y } = getPos(e);
+  lastX = x;
+  lastY = y;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (!drawing) return;
+  const { x, y } = getPos(e);
+
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(x, y);
+  ctx.stroke();
+
+  lastX = x;
+  lastY = y;
+});
+
+// prevent drawing out of canvas
+canvas.addEventListener("mouseup", () => {
+  drawing = false;
+});
+
+canvas.addEventListener("mouseleave", () => {
+  drawing = false;
+});
